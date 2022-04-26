a5bbef16bea17d5ee78a4b9c89077a65e765886b
semvlu <74308718+semvlu@users.noreply.github.com>
2022-04-26 15:13:55 +0800

    Add files via upload

---
+++ DFS/SPath.cpp
@@ -0,0 +1,34 @@
+﻿#include "SPath.h"
+using namespace std;
+
+int main()
+{
+	vector <vector <char> > a(10, vector <char>(10));
+	for (int i = 0; i < a.size(); i++)
+	{
+		for (int j = 0; j < a[i].size(); j++)
+			a[i][j] = ' ';
+	}
+	for (int i = 0; i < a.size(); i++)
+	{
+		for (int j = 0; j < a[i].size(); j++)
+		{
+			if (i == 0 || j == 0)
+				a[i][j] = 'W';
+			else if (i == a.size() - 1 || j == a[i].size() - 1)
+				a[i][j] = 'W';
+		}
+	}
+	for (int i = 3; i < 9; i++)
+		a[4][i] = 'W';
+	for (int i = 2; i < 7; i++)
+		a[i][5] = 'W';
+	a[2][3] = 's'; // Start
+	a[6][9] = ' '; // Finish
+	show(a);
+
+	dfs(a);
+	cout << endl;
+	show(a);
+	return 0;
+}
---
+++ DFS/SPath.h
@@ -0,0 +1,203 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+void show(vector <vector <char> > a)
+{
+	for (int i = 0; i < a.size(); i++)
+	{
+		for (int j = 0; j < a[i].size(); j++)
+			cout << a[i][j];
+		cout << endl;
+	}
+}
+
+void dfs(vector <vector <char> > a)
+{
+	int i = 0;
+	int j = 0;
+	while (i < a.size() - 1)
+	{
+		while (j < a[i].size() - 1)
+		{
+			if (a[i][j] != ' ' && a[i][j] != 'W')
+			{
+				while (a[i + 1][j] == ' ' || a[i - 1][j] == ' '
+					|| a[i][j + 1] == ' ' || a[i][j - 1] == ' ')
+				{
+					if (a[i + 1][j] == ' ')
+					{
+						switch (a[i][j])
+							{
+							case 's':
+								a[i + 1][j] = '1';
+								break;
+							case '1':
+								a[i + 1][j] = '2';
+								break;
+							case '2':
+								a[i + 1][j] = '3';
+								break;
+							case '3':
+								a[i + 1][j] = '4';
+								break;
+							case '4':
+								a[i + 1][j] = '5';
+							case '5':
+								a[i + 1][j] = '6';
+								break;
+							case '6':
+								a[i + 1][j] = '7';
+								break;
+							case '7':
+								a[i + 1][j] = '8';
+								break;
+							case '8':
+								a[i + 1][j] = '9';
+								break;
+							case '9':
+								a[i + 1][j] = '0';
+								break;
+							case '0':
+								a[i + 1][j] = '1';
+								break;
+							default:
+								break;
+							}
+					}
+					else if (a[i - 1][j] == ' ')
+					{
+						switch (a[i][j])
+							{
+							case 's':
+								a[i - 1][j] = '1';
+								break;
+							case '1':
+								a[i - 1][j] = '2';
+								break;
+							case '2':
+								a[i - 1][j] = '3';
+								break;
+							case '3':
+								a[i - 1][j] = '4';
+								break;
+							case '4':
+								a[i - 1][j] = '5';
+							case '5':
+								a[i - 1][j] = '6';
+								break;
+							case '6':
+								a[i - 1][j] = '7';
+								break;
+							case '7':
+								a[i - 1][j] = '8';
+								break;
+							case '8':
+								a[i - 1][j] = '9';
+								break;
+							case '9':
+								a[i - 1][j] = '0';
+								break;
+							case '0':
+								a[i - 1][j] = '1';
+								break;
+							default:
+								break;
+							}
+					}
+					else if (a[i][j + 1] == ' ')
+					{
+						switch (a[i][j])
+							{
+							case 's':
+								a[i][j + 1] = '1';
+								break;
+							case '1':
+								a[i][j + 1] = '2';
+								break;
+							case '2':
+								a[i][j + 1] = '3';
+								break;
+							case '3':
+								a[i][j + 1] = '4';
+								break;
+							case '4':
+								a[i][j + 1] = '5';
+							case '5':
+								a[i][j + 1] = '6';
+								break;
+							case '6':
+								a[i][j + 1] = '7';
+								break;
+							case '7':
+								a[i][j + 1] = '8';
+								break;
+							case '8':
+								a[i][j + 1] = '9';
+								break;
+							case '9':
+								a[i][j + 1] = '0';
+								break;
+							case '0':
+								a[i][j + 1] = '1';
+								break;
+							default:
+								break;
+							}
+					}
+					else if (a[i][j - 1] == ' ')
+					{
+						switch (a[i][j])
+							{
+							case 's':
+								a[i][j - 1] = '1';
+								break;
+							case '1':
+								a[i][j - 1] = '2';
+								break;
+							case '2':
+								a[i][j - 1] = '3';
+								break;
+							case '3':
+								a[i][j - 1] = '4';
+								break;
+							case '4':
+								a[i][j - 1] = '5';
+							case '5':
+								a[i][j - 1] = '6';
+								break;
+							case '6':
+								a[i][j - 1] = '7';
+								break;
+							case '7':
+								a[i][j - 1] = '8';
+								break;
+							case '8':
+								a[i][j - 1] = '9';
+								break;
+							case '9':
+								a[i][j - 1] = '0';
+								break;
+							case '0':
+								a[i][j - 1] = '1';
+								break;
+							default:
+								break;
+							}
+					}
+
+				}
+			}
+			else
+			{
+				i++;
+				j++;
+			}
+		}	
+	}
+}
