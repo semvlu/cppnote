8caacc3866fa61d6164d6c50423ff188ed646170
semvlu <74308718+semvlu@users.noreply.github.com>
2022-05-31 14:38:15 +0800

    Add files via upload

--- note.cpp
+++ note.cpp
@@ -8,12 +8,28 @@ using namespace std;
 
 int main()
 {
+// references
+    int *p1, *p2;
+    p1 = new int;
+    *p1 = 77;
+    p2 = p1;
+    *p2 = 12; // *p1 = *p2 = 12
+
+    int c = 7;
+    int &d = c; // d = c, &d = &c
+    d = 66; // c = d = 66
+
 // data type conversion
     int b = static_cast <int> (2.5);
 
 // variable length array
     int *ar;
     ar = new int[n];
+    vector <vector <int> > c;
+    c.resize(m);
+    for (int i = 0; i < m; i++)
+        c[i].resize(n);
+
 // dynamic array memory clear
     delete [] ar;
     ar = nullptr;
@@ -43,6 +59,7 @@ int main()
         cout << endl;
     }
 
+// map
     map <string, int> m;
     m["London"] = 0;
     m["Paris"] = 1;
@@ -73,4 +90,21 @@ int main()
     char c4 = s.get();
     cout << c1 << endl << c2 << endl
          << c3 << endl << c4 << endl;
+// iterator
+    vector <int> a;
+    for (int i = 0; i < 10; ++i)
+        a.push_back(rand());
+    for (int i = 0; i < 10; ++i)
+        cout << a[i] << " ";
+
+    vector <int>::iterator p;
+    for (p = a.begin(); p < a.end(); ++p)
+        cout << *p << " ";
+
+    auto it = a.begin();
+    cout << it[2] << " " << *(it + 2);
+
+    for(auto i: a)
+        cout << i << " ";
+
 }
--- note.h
+++ note.h
@@ -98,11 +98,143 @@ public:
 
 PFArrayD b(20);
 for ( int i = 0; i < 20; i++)
-    b.addElement(i);
+b.addElement(i);
 PFArrayD temp(b);
 class Position
 {
 public:
     Position(const Position&) = default;
 };
+
+// operator
+class Matrix
+{
+public:
+    int row;
+    int clmn;
+    vector <vector <int> > elem;
+
+    Matrix(int m , int n)
+    {
+        row = m;
+        clmn = n;
+        elem.resize(m);
+        for (int i = 0; i < elem.size(); i++)
+            elem[i].resize(n);
+    }
+
+    Matrix(vector < vector<int> > e)
+    {
+        elem.resize(e.size());
+        for (int i = 0; i < elem.size(); i++)
+            elem[i].resize(e[i].size());
+
+        for (int i = 0; i < elem.size(); i++)
+        {
+            for (int j = 0; j < elem[i].size(); j++)
+                elem[i][j] = e[i][j];
+        }
+    }
+
+    int getElem(Matrix a, int i, int j) { return a.elem[i][j]; }
+    int getRow() { return row; }
+    int getClmn() { return clmn; }
+// member (unary)
+    Matrix operator+(Matrix& a)
+    {
+        vector <vector <int> > c;
+        c.resize(elem.size());
+        for (int i = 0; i < elem.size(); i++)
+            c[i].resize(elem[i].size());
+
+        for (int i = 0; i < a.elem.size(); i++)
+        {
+            for (int j = 0; j < a.elem[i].size(); j++)
+                c[i][j] = elem[i][j] + a.elem[i][j];
+        }
+        return Matrix(c);
+    }
+// non-member (binary)
+    friend int operator*(Matrix a, Matrix b);
+};
+// or assign member outside the class
+Matrix Matrix::operator+(Matrix a){}
+// non-member
+int operator*(Matrix a, Matrix b){}
+bool operator==(Matrix a, Matrix b) // bool: > < ==
+{
+    return (a.getRow == b.getRow()
+            && a.getClmn == b.getClmn());
 }
+ostream& operator<<(ostream& strm, const Matrix& a)
+{
+    int r = a.row;
+    for (int i = 0; i < r; i++)
+        strm << v.value[i] << " ";
+    return strm;
+}
+// prefix & postfix increment
+INT operator++()
+{
+    INT temp;
+    temp.i = ++i;
+    return temp;
+}
+INT operator++(int)
+{
+    INT temp;
+    temp.i = i++;
+    return temp;
+}
+int &Array::operator[](int idx) { return ar[idx]; }
+
+// inheritance & polymorphism (virtual/ override)
+class Obj
+{
+public:
+    string a;
+    int b;
+    Obj(string c, int i)
+    {
+        a = c;
+        b = i;
+    }
+
+    virtual void print() = 0;
+};
+class vir : public Obj
+{
+public:
+    vir(string c, int i) : Obj(c, i)
+    {
+        a = c;
+        b = i;
+    }
+    void print() override
+    {
+        cout << "THis is a child func from parent virtual func!!" << endl;
+    }
+};
+
+template <class T>
+void swap(T var1, T var2)
+{
+    T temp;
+    temp = var1;
+    var1 = var2;
+    var2 = temp;
+}
+
+// template
+template <class T>
+void swapVal(T var1, T var2)
+{
+    T temp;
+    temp = var1;
+    var1 = var2;
+    var2 = temp;
+}
+
+template <const int *pci> struct X{};
+int ai[10];
+X<ai> xi;
